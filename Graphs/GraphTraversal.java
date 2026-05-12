122877eef09498ece3c9a15e6ef5a20845fdbec6
Y. Kaushal Mohan Kumar <kaushalcollegeacc@gmail.com>
2026-05-13 01:48:48 +0530

    crazy.

--- Graphs/GraphTraversal.java
+++ Graphs/GraphTraversal.java
@@ -1,7 +1,7 @@
 // package Graphs;
 import java.util.*;
 
-public class Graph {
+public class GraphTraversal {
 
     public static class Edge {
         // Attributes of the Edge class.
@@ -40,6 +40,57 @@ public class Graph {
 
         graph[3].add(new Edge(3, 1));
         graph[3].add(new Edge(3, 2));
+    }
+
+
+    private static void bfs (List<Edge>[] g, boolean[] v, int V, int idx) {
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        q.add(idx);
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+
+            if (v[curr] == false) {
+                System.out.print(curr + " ");
+                v[curr] = true;
+                for (int x = 0; x < g[curr].size(); x++) q.add(g[curr].get(x).dest);
+            }
+        }
+    }
+
+    private static void dfs (List<Edge>[] g, int curr, boolean[] v) {
+        System.out.print(curr + " ");
+        v[curr] = true;
+
+        for (int x = 0; x < g[curr].size(); x++) if(v[g[curr].get(x).dest] == false) dfs(g, g[curr].get(x).dest, v);
+    }
+
+    public static void createComplexGraph(List<Edge>[] complexGraph) {
+        for (int x = 0; x < complexGraph.length; x++) complexGraph[x] = new ArrayList<Edge>();
+
+        complexGraph[0].add(new Edge(0, 1));
+        complexGraph[0].add(new Edge(0, 2));
+
+        complexGraph[1].add(new Edge(1, 0));
+        complexGraph[1].add(new Edge(1, 3));
+
+        complexGraph[2].add(new Edge(2, 0));
+        complexGraph[2].add(new Edge(2, 4));
+
+        complexGraph[3].add(new Edge(3, 1));
+        complexGraph[3].add(new Edge(3, 4));
+        complexGraph[3].add(new Edge(3, 5));
+
+        complexGraph[4].add(new Edge(4, 2));
+        complexGraph[4].add(new Edge(4, 3));
+        complexGraph[4].add(new Edge(4, 5));
+
+        complexGraph[5].add(new Edge(5, 3));
+        complexGraph[5].add(new Edge(5, 4));
+        complexGraph[5].add(new Edge(5, 6));
+
+        complexGraph[6].add(new Edge(6, 5));
     }
 
     public static void createWeightedGraph (List<WeightedEdge>[] weightedGraph) {
@@ -83,5 +134,14 @@ public class Graph {
                 System.out.println("dest = " + weightedGraph[x].get(y).dest + ", wt = " + weightedGraph[x].get(y).wt);
             }
         }
+
+        V = 7;
+        System.out.println();
+
+        List<Edge>[] complexGraph = new ArrayList[V];
+        createComplexGraph(complexGraph);
+        boolean[] vis = new boolean[V];
+
+        for (int x = 0; x < V; x++) if (vis[x] == false) bfs(complexGraph, vis, V, x);
     }
 }
